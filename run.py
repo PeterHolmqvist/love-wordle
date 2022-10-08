f1a9a99051a83ed8e688df896bc3c820b8bd3400
PeterHolmqvist <peterholmqvist@hotmail.com>
2022-10-09 01:41:39 +0200

    add guess input and ceck if correct for loops

--- run.py
+++ run.py
@@ -4,12 +4,43 @@ import sys
 from termcolor import colored
 from list import word_list
 
-#for loop to iterate words of words_list and return lowercase
+#for loop to iterate words of words_list and return teh word lowercase.
 for i in range(len(word_list)):
     word_list[i] = word_list[i].lower()
 
 
-# function that chooses random word from the words list
+# function that chooses random word from the words list.
 words = word_list
 word = random.choice(words)
 print(word)
+
+# Start game instructions press "" to start game.
+welcome = input("Welcome to Python Wordl game! \n \n Instructions: \n \n You get 6 tries to guess the correct 5 letter word. \n If a letter turn up green it means it is in the right place \n if a letter turn upp yellow it means that is in the word but in another place. \n If your letter turn up red it is the wrong letter! \n \n GLHF! \n \n Press 'Enter' to continue! \n \n")
+
+#input to be displayed through out the game
+def game_display():
+    print('please guess a 5 letter word and press "Enter"!')
+    
+
+game_display()
+
+for tries in range(1, 7):
+    guess = input().lower()
+
+    if len(guess) > 5:
+        print(colored("Your word is too long, only 5 letters please!\n", 'red'), end="")
+    elif len(guess) < 5:
+        print(colored("Your word is too short, 5 letters please!\n", 'red'), end="")
+    if not guess.isalpha():
+        print(colored("Only letters are allowed!", 'red'), end="")
+
+    for i in range(5):
+        if guess[i] == word[i]:
+            print(colored(guess[i], 'green'), end="")
+        elif guess[i] in word:
+            print(colored(guess[i], 'yellow'), end="")
+        else:
+            print(guess[i], end="")
+
+        if guess == word:
+            print(colored(f"Awsome! You gussed the the right word in {tries} tries.", 'red'))
