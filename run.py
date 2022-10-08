9fe80001ce1d0c0e9373f77ac376491b9a871de2
PeterHolmqvist <peterholmqvist@hotmail.com>
2022-10-09 00:26:47 +0200

    added list and random function

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
 #Imports
 import random
 import sys
-from termcolor import color
+from termcolor import colored
 from list import word_list
+
+#for loop to iterate words of words_list and return lowercase
+for i in range(len(word_list)):
+    word_list[i] = word_list[i].lower()
+
+
+# function that chooses random word from the words list
+words = word_list
+word = random.choice(words)
+print(word)
