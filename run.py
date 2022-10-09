8037fcd5648916c92c57a43d560fdccec7de3c39
PeterHolmqvist <peterholmqvist@hotmail.com>
2022-10-09 14:31:30 +0200

    try to debugg and deploy

--- run.py
+++ run.py
@@ -1,48 +1,36 @@
-#Imports
 import random
 import sys
 from termcolor import colored
 from list import word_list
 
-#for loop to iterate words of words_list and return teh word lowercase.
 for i in range(len(word_list)):
     word_list[i] = word_list[i].lower()
 
-
-
-
-# Start game instructions press "Enter" to start game.
 welcome = input("Welcome to Python Wordl game! \n \n Instructions: \n \n You get 6 tries to guess the correct 5 letter word. \n If a letter turn up green it means it is in the right place \n if a letter turn upp yellow it means that is in the word but in another place. \n If your letter turn up red it is the wrong letter! \n \n GLHF! \n \n Press 'Enter' to continue! \n \n")
 
-#input to be displayed through out the game
 def game_display():
     print('please guess a 5 letter word and press "Enter"!\n')
-    
-    
 
 replay_game = ""
-
-#while loop to exit or replay game.
 while replay_game != "q":
-    # function that chooses random word from the words list.
     words = word_list
     word = random.choice(words)
     print(word)
     game_display()
     for tries in range(1, 7):
-        guess = input().lower()
+        guess = input().lower
 
         sys.stdout.write('\x1b[1A')
         sys.stdout.write('\x1b[2K')
 
         if len(guess) > 5:
-            print(colored("Your word is too long, only 5 letters please!\n", 'red'), end="")
+            print(colored("Your word is too long, only 5 letters please!\n", 'red'), end="",)
         elif len(guess) < 5:
             print(colored("Your word is too short, 5 letters please!\n", 'red'), end="")
         if not guess.isalpha():
             print(colored("Only letters are allowed!", 'red'), end="")
 
-        for i in range(len(guess), len(guess)):
+        for i in range(0,6):
             if guess[i] == word[i]:
                 print(colored(guess[i], 'green'), end="")
             elif guess[i] in word:
@@ -54,6 +42,5 @@ while replay_game != "q":
         if guess == word:
             print(colored(f"Awsome! You gussed the the right word in {tries} tries.", 'green'))
             break
-        elif tries == 6:
-            print(f"Game over! The correct word was {word}.")
-    replay_game = input("Press any key to replay or press q to quit")    
+    replay_game = input("Press any key to replay or press q to quit")
+    
